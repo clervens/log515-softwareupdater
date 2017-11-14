96cb5ee54e2d4f459d894470167b9f6ec8e571dc
Clervens Volcy <clervens.volcy@gmail.com>
2017-11-14 15:34:31 -0500

    Change node creation to lodash template

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
-const ApplicationManager = require('../model/ApplicationManager');
+const ApplicationManager = require('../model/ApplicationManager'),
+    _ = require('lodash');
 let applicationList = [];
 
 let body = document.getElementsByTagName('body')[0];
@@ -11,53 +12,30 @@ ApplicationManager.get().then((appList) => {
   let htmlList = document.getElementById("applicationList");
   document.getElementById("loading").style.display = 'none';
 
+$(htmlList).on('click', 'li .versionavailable button', (event) => {
+    let $progress = $(event.target).parents('li').find('.progress');
+    let $progressBar = $progress.find('.progress-bar');
+    $progress.css('display', 'block');
+
+    // Change for loop until completed.
+    $progressBar.css('width', '90%');
+
+    let appId = $(event.target).parents('li').attr('id');
+
+   console.log("onclick: " + appId);
+});
+
+  let appListTemplate = _.template(document.querySelector('#AppListTemplate').textContent);
   for(let i=0; i< appList.length; i++){
       let el = appList[i];
       if(!el || !el.Name) continue;
-      let li = document.createElement("li");
-      el.appId = el.Name.replace(/\s+/g, '_');
-      li.id = el.appId;
+      
+      let appItem = appListTemplate({
+          appId: el.Name.replace(/\s+/g, '_'),
+          Name: el.Name,
+          Version: el.Version
+      });
 
-      let divName = document.createElement("div");
-      divName.classList.add("appname");
-      let text = document.createTextNode(el.Name);
-      divName.appendChild(text);
-
-      let divVersion = document.createElement("div");
-      divVersion.classList.add("appversion");
-      text = document.createTextNode(el.Version);
-      divVersion.appendChild(text);
-
-      let progress = document.createElement("div");
-      progress.classList.add("progress");
-      progress.style.display = 'none';
-
-      let progressBar = document.createElement("div");
-      progressBar.classList.add("progress-bar");
-      progressBar.setAttribute("role", "progressbar");
-      progressBar.setAttribute("aria-valuenow", "0");
-      progressBar.setAttribute("aria-valuemin", "0");
-      progressBar.setAttribute("aria-valuemax", "100");
-      progressBar.style.width = 0;
-
-      progress.appendChild(progressBar);
-
-      let updateInfo = document.createElement("span");
-      updateInfo.classList.add("versionavailable");
-      divVersion.appendChild(updateInfo);
-
-      let btn = document.createElement("button");
-      btn.onclick = function() {
-         progress.style.display = 'block';
-         //example
-        progressBar.style.width = '90%';
-        console.log("onclick:" + el.appId);
-      };
-      updateInfo.appendChild(btn);
-
-      li.appendChild(divName);
-      li.appendChild(divVersion);
-      li.appendChild(progress);
-      htmlList.appendChild(li);
+      $(htmlList).append(appItem);
   }
 }).catch((e) => {/* On error do nothing */ console.log(e);});
