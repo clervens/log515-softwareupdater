bdcc790988569e8c6930a42c77813e22b23172b4
willkoua <willkoua@gmail.com>
2017-11-14 20:01:52 -0500

    add json file

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ const mb = require('menubar')({
   index: 'file://' + path.join(__dirname, 'index.html')
 });
 
+var fs = require("fs");
+var content = fs.readFileSync("data.json");
+var jsonContent = JSON.parse(content);
+
 mb.on('ready', () => {
-  console.log('Menubar electron app is ready')
+  console.log('Menubar electron app is ready');
+
+    for(var i=0; i<jsonContent.length; i++) {
+        console.log(jsonContent[i].Name);
+    }
 });
 
 mb.on('window-all-closed', () => {
-  mb.quit()
+  mb.quit();
 });
