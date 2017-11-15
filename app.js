d7abca586a774644ea6c2f1576d05a3371b939b4
clervens <clervens.volcy@gmail.com>
2017-11-15 13:14:52 -0500

    Change app icon to be visible in the taskbar.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mb = require('menubar')({
   preloadWindow: true,
   width: 340,
   height: 540,
-  index: 'file://' + path.join(__dirname, 'views', 'index.html')
+  index: 'file://' + path.join(__dirname, 'views', 'index.html'),
+  icon: path.join(__dirname, 'assets', 'img', 'if_Download_drive_data_data_storage_hdd_hard_to_1886949.png')
 });
 
 mb.on('ready', () => {
