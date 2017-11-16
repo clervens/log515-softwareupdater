96fd80329d45628fd3a54fd153b344db3ca2bf79
Jeremy El bez <jeremy.lb@outlook.com>
2017-11-16 15:59:29 -0500

    remove old tests

--- app.js
+++ app.js
@@ -9,19 +9,11 @@ const mb = require('menubar')({
   icon: path.join(__dirname, 'assets', 'img', 'if_cat_upsidedown_185521.png')
 });
 
-var fs = require("fs");
-var content = fs.readFileSync("data.json");
-var jsonContent = JSON.parse(content);
-
 mb.on('ready', () => {
   console.log('Menubar electron app is ready');
   globalShortcut.register('CommandOrControl+R', () => {
     mb.window.reload();
   });
-
-    for(var i=0; i<jsonContent.length; i++) {
-        console.log(jsonContent[i].Name);
-    }
 });
 
 mb.on('window-all-closed', () => {
